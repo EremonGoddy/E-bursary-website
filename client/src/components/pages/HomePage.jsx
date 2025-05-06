12ef431b17df77d4aa191173b4446c472a435ec9
EremonGoddy <eremon.godwin@gmail.com>
2025-05-06 09:10:31 +0300

    update new

--- client/src/components/pages/HomePage.jsx
+++ client/src/components/pages/HomePage.jsx
@@ -123,12 +123,12 @@ const HomePage = () => {
               </div>
               {/* Centered Button */}
               <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <button
+      <Link
         className="getting-started"
-        onClick={() => (window.location.href = "/login")}
+        to="/login"
       >
         Get Started
-      </button>
+      </Link>
     </div>
             </div>
           ))}
