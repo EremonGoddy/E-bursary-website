90b4d3b262a228305de1cd5a2365423334128cdc
EremonGoddy <eremon.godwin@gmail.com>
2025-05-20 17:07:39 +0300

    update

--- client/src/components/authentication/LoginPage.jsx
+++ client/src/components/authentication/LoginPage.jsx
@@ -4,120 +4,119 @@ import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import "./Authentication.css";
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState({});
-  const [showPassword, setShowPassword] = useState(false);
-  const [rememberMe, setRememberMe] = useState(false);
-  const navigate = useNavigate();
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [errors, setErrors] = useState({});
+const [showPassword, setShowPassword] = useState(false);
+const [rememberMe, setRememberMe] = useState(false);
+const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newErrors = {};
-    if (!email) newErrors.email = '*Please provide an email';
-    if (!password) newErrors.password = '*Please provide a password';
-    if (Object.keys(newErrors).length > 0) {
-      setErrors(newErrors);
-      return;
-    }
+const handleSubmit = (e) => {
+e.preventDefault();
+const newErrors = {};
+if (!email) newErrors.email = '*Please provide an email';
+if (!password) newErrors.password = '*Please provide a password';
+if (Object.keys(newErrors).length > 0) {
+setErrors(newErrors);
+return;
+}
 
-    axios
-      .post('https://e-bursary-backend.onrender.com/api/signin', { email, password })
-      .then((response) => {
-        const { token, student, role, name } = response.data;
-        const storage = rememberMe ? localStorage : sessionStorage;
-        storage.setItem('authToken', token);
-        storage.setItem('student', JSON.stringify(student));
-        storage.setItem('userName', name); // Save user's name in session storage
+axios
+.post('https://e-bursary-backend.onrender.com/api/signin', { email, password })
+.then((response) => {
+const { token, student, role, name } = response.data;
+const storage = rememberMe ? localStorage : sessionStorage;
+storage.setItem('authToken', token);
+storage.setItem('student', JSON.stringify(student));
+storage.setItem('userName', name); // Save user's name in session storage
 
-        if (role === 'Student') navigate('/studentdashboard');
-        else if (role === 'Admin') navigate('/admindashboard');
-        else if (role === 'Committee') navigate('/committeedashboard');
-        else alert('Role not recognized');
-      })
-      .catch((err) => alert(err.response.data.message));
-  };
+if (role === 'Student') navigate('/studentdashboard');
+else if (role === 'Admin') navigate('/admindashboard');
+else if (role === 'Committee') navigate('/committeedashboard');
+else alert('Role not recognized');
+})
+.catch((err) => alert(err.response.data.message));
+};
 
-  const togglePasswordVisibility = () => {
-    setShowPassword((prevState) => !prevState);
-  };
+const togglePasswordVisibility = () => {
+setShowPassword((prevState) => !prevState);
+};
 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white shadow-lg rounded-lg p-8 w-11/12 max-w-md">
-        <h2 className="text-2xl font-bold text-center mb-6">Sign in</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-lg font-medium text-gray-700 mb-2">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Enter your email"
-            />
-            {errors.email && <div className="text-red-500 text-sm mt-1">{errors.email}</div>}
-          </div>
+return (
+<div className="flex items-center justify-center min-h-screen">
+<div className="bg-white rounded-lg p-8 w-11/12 max-w-md shadow-[0_4px_8px_rgba(0,0,0,0.6)]">
+<h2 className="text-2xl md:text-3xl font-bold text-center mb-6">Sign in</h2>
+<form onSubmit={handleSubmit}>
+<div className="mb-4">
+<label htmlFor="email" className="block text-gray-700 text-[1rem] md:text-[1.1rem] font-semibold mb-1">Email</label>
+<input
+type="email"
+id="email"
+name="email"
+className="w-full px-4 py-2 border text-[1rem] md:text-[1.1rem]  border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-700"
+value={email}
+onChange={(e) => setEmail(e.target.value)}
+placeholder="Enter your email"
+/>
+{errors.email && <div className="text-red-500 text-sm mt-1">{errors.email}</div>}
+</div>
 
-          <div className="mb-4 relative">
-            <label htmlFor="password" className="block text-lg font-medium text-gray-700 mb-2">Password</label>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              name="password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter your password"
-            />
-            <span
-              className=" eye absolute inset-y-0 right-3 inline-flex items-center cursor-pointer text-gray-500"
-              onClick={togglePasswordVisibility}
-              style={{
-                display: 'inline-flex',
-                width: 'auto',
-                padding: '0.5rem',
-              }}
-            >
-              <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
-            </span>
-            {errors.password && <div className="text-red-500 text-sm mt-1">{errors.password}</div>}
-          </div>
+<div className="mb-4 relative">
+<label htmlFor="password" className="block text-gray-700 text-[1rem] md:text-[1.1rem] font-semibold mb-1">Password</label>
+<input
+type={showPassword ? 'text' : 'password'}
+id="password"
+name="password"
+className="w-full px-4 py-2 border text-[1rem] md:text-[1.1rem]  border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-700"
+value={password}
+onChange={(e) => setPassword(e.target.value)}
+placeholder="Enter your password"
+/>
+<span
+className=" text-[1rem] md:text-[1.6rem] mt-10 md:mt-7 absolute inset-y-0 right-3 inline-flex items-center cursor-pointer text-gray-500"
+onClick={togglePasswordVisibility}
+style={{
+display: 'inline-flex',
+width: 'auto',
+padding: '0.5rem',
+}}
+>
+<FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+</span>
+{errors.password && <div className="text-red-500 text-sm mt-1">{errors.password}</div>}
+</div>
 
-          <div className="mb-4 flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                id="rememberMe"
-                name="rememberMe"
-                className="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
-                checked={rememberMe}
-                onChange={(e) => setRememberMe(e.target.checked)}
-              />
-              <label htmlFor="rememberMe" className="ml-2 text-sm text-gray-700">Remember me</label>
-            </div>
-            <Link to="/forgotpassword" className="text-sm text-blue-500 hover:underline">Forgot password?</Link>
-          </div>
+<div className="mb-4 flex items-center justify-between">
+<div className="flex items-center">
+<input
+type="checkbox"
+id="rememberMe"
+name="rememberMe"
+className="h-4 w-4 md:h-4.5 md:w-4.5 text-gray-600 border-gray-300 rounded focus:ring-gray-500"
+checked={rememberMe}
+onChange={(e) => setRememberMe(e.target.checked)}
+/>
+<label htmlFor="rememberMe" className="ml-2 text-[1rem] md:text-[1.1rem] text-gray-700">Remember me</label>
+</div>
+<Link to="/forgotpassword" className="text-[1rem] md:text-[1.1rem] text-gray-700 hover:underline">Forgot password?</Link>
+</div>
 
-          <button
-            type="submit"
-            className="buttonsignin w-full  py-2 rounded-lg  transition"
-          >
-            Sign in
-          </button>
+<button
+type="submit"
+className="text-[1rem] md:text-[1.1rem] w-full text-white  py-2 rounded-lg bg-gray-700  transition"
+>
+Sign in
+</button>
 
-          <div className="text-center mt-4">
-            <span className="text-sm text-gray-600">Don't have an account? </span>
-            <Link to="/register" className="text-sm text-blue-500 hover:underline">Create an Account</Link>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+<div className="text-center mt-4">
+<span className="text-[1rem] md:text-[1.1rem] text-gray-600">Don't have an account? </span>
+<Link to="/register" className="text-[1rem] md:text-[1.1rem] text-blue-500 hover:underline">Create an Account</Link>
+</div>
+</form>
+</div>
+</div>
+);
 };
 
 export default LoginPage;
