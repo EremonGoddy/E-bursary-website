ce5f599c97d7753b164430935f8a0c218d0c7d13
EremonGoddy <eremon.godwin@gmail.com>
2025-07-13 14:36:38 +0300

    update

--- client/src/components/authentication/RegisterPage.jsx
+++ client/src/components/authentication/RegisterPage.jsx
@@ -79,7 +79,7 @@ const RegisterPage = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="bg-white rounded-lg p-8 w-11/12 max-w-md shadow-[0_0_10px_3px_rgba(0,0,0,0.25)]">
+      <div className="bg-white rounded-lg p-8 w-11/12 max-w-md -mt-2 md:mt-0 shadow-[0_0_10px_3px_rgba(0,0,0,0.25)]">
         <h2 className="text-[#14213d] text-2xl md:text-3xl font-bold text-center mb-6">Create Account</h2>
         <form onSubmit={handleSubmit}>
           {/* Full Name */}
