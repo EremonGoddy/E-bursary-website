f8dd37670bd0a8038533642c45bf26bfb043a702
EremonGoddy <eremon.godwin@gmail.com>
2026-08-27 13:08:08 +0300

    update

--- client/src/components/authentication/RegisterPage.jsx
+++ client/src/components/authentication/RegisterPage.jsx
@@ -24,7 +24,7 @@ email: '',
 phoneNumber: '',
 password: '',
 confirmPassword: '',
-role: 'Student',
+role: 'Committee',
 });
 const [errors, setErrors] = useState({});
 const [isTermsAccepted, setIsTermsAccepted] = useState(false);
